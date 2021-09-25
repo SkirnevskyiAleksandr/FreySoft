472fbddd2421a9707a964c0ccebb65a6c05e171e
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-09-26 00:20:55 +0300

    init:add-gulp-functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
-// const { on } = require('gulp');
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass')(require('sass'));
 
 function css_style(done){
-    gulp.src('./scss/style.scss')
+    gulp.src('./scss/**/*')
         .pipe(sass({
             errorLogToConsole: true,
             outputStyle:'compressed'
         }))
         .on('error', console.error.bind(console))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./css/'));
-    done();    
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+    done();     
+} 
+
+function sync(done){
+    browserSync.init({
+        server:{
+            baseDir:'./'
+        },
+        port:3000
+    })
+}
+// exports.default = css_style;
+
+function watchSass(){
+    gulp.watch('./scss/**/*',css_style)
+}
+function watchFiles(){
+    gulp.watch('./scss/**/*',css_style)
+    gulp.watch('./**/*.html',browserRelod)
+    gulp.watch('./**/*.js',browserRelod)
+}
+function browserRelod(done){
+    browserSync.reload();
+    done();
 }
 
-gulp.task(css_style);
+gulp.task("default", gulp.parallel(sync, watchFiles));
+gulp.task(sync);
